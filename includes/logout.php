15eee85d281365d4da61092bd84a7204e270c6e1
joyritaa <joyritanjoki061@gmail.com>
2025-03-10 12:05:38 +0300

    Remove legacy PHP scripts and update logout button functionality in user view

--- includes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// logout.php
-session_start();   // Start or resume the current session
-session_destroy(); // Destroy all session data
-
-// Redirect the user to the login page
-header("Location: login.html");
-exit();
-?>
